96467823f050d60b51718ddaf4e22baee5a33c04
Abduraxim <toxirmalk@gmail.com>
2024-12-02 16:22:51 +0500

    Bugfix

--- src/services/otp.service.js
+++ src/services/otp.service.js
@@ -1,41 +1,37 @@
-import { logger } from "../utils/index.js";
-import { connection } from "../database/index.js";
+import {connection} from "../Database/index.js"
+import {logger} from "../utils/logger/logger.js"
 
-export const saveOtp = async (otp) => {
-  try {
-    await connection("otp").insert({ otp });
+export const SaveOtp=async(otp)=>{
+    try {
+        await connection('otp').insert({otp})
+        logger.info("Otp saved")
+    } catch (error) {
+        logger.error(error.message)
+    }
+}
 
-    logger.info("Otp saved");
-  } catch (error) {
-    logger.error(error);
-  }
-};
+export const findByotp=async(otp)=>{
+    try {
+        const res=await connection.select("*").from('otp').where({otp})
+        if(res.length>=1){
+            return res
+        }else{
+            return "Otp topilmadi"
+        }
+    } catch (error) {
+        return error.message
+    }
+}
 
-export const findByOtp = async (otp) => {
-  try {
-    const res = await connection.select("*").from("otp").where({ otp });
-
-    if (res.length >= 1) return res;
-
-    return "Otp topilmadi";
-  } catch (error) {
-    return error;
-  }
-};
-
-export const deleteOtp = async (otp) => {
-  try {
-    const res = await connection
-      .select("*")
-      .from("otp")
-      .where({ otp })
-      .returning("*")
-      .del();
-
-    if (res.length >= 1) return "Otp o'chirildi";
-
-    return "Otp topilmadi";
-  } catch (error) {
-    return error;
-  }
-};
+export const deleteOtp=async(otp)=>{
+    try {
+        const res=await connection.select("*").from('otp').where({otp}).returning("*").del()
+        if(res.length>=1){
+            return "Otp o'chirildi"
+        }else{
+            return "Otp topilmadi"
+        }
+    } catch (error) {
+        return error.message
+    }
+}
